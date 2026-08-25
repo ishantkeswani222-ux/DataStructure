c1229296c72ab277a49e1f52407711dd813b5481
Ishant keswani <ishantkeswani222@gmail.com>
2026-08-25 16:35:43 +0500

    Add Stack display

--- src/Stack/DisplayStack.java
+++ src/Stack/DisplayStack.java
@@ -3,6 +3,22 @@ package Stack;
 import java.util.Stack;
 
 public class DisplayStack {
+    // other method to display in reverse
+    public static void displayRevRec(Stack<Integer> st){
+        if(st.size()==0) return;
+        int top = st.pop();
+        System.out.print(top+" ");
+        displayRevRec(st);
+        st.push(top);
+    }
+    // other method to display in order
+    public static void display(Stack<Integer> st){
+        if(st.size()==0) return;
+        int top = st.pop();
+        display(st);
+        System.out.print(top+" ");
+        st.push(top);
+    }
     static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(1);
@@ -11,14 +27,18 @@ public class DisplayStack {
         st.push(4);
         st.push(5);
         st.push(6);
-        Stack<Integer> st2 = new Stack<>();
-        while(st.size()>0){
-            st2.push(st.pop());
-        }
-        while(st2.size()>0){
-            int x = st2.pop();
-            System.out.print(x+" ");
-            st.push(x);
-        }
+        // one method
+//        Stack<Integer> st2 = new Stack<>();
+//        while(st.size()>0){
+//            st2.push(st.pop());
+//        }
+//        while(st2.size()>0){
+//            int x = st2.pop();
+//            System.out.print(x+" ");
+//            st.push(x);
+//        }
+        //displayRevRec(st);
+        display(st);
+
     }
 }
